d6bc742bb0285cc51ffce079b79d77794309f200
scottberke <scottberke@gmail.com>
2016-03-04 15:18:52 -0700

    Adds amount boxes and delete function
    - Adds boxes that track total credit, total debit
    and balance
    - Adds delete button and corresponding functionality
    to the backend

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -5,4 +5,25 @@ class RecordsController < ApplicationController
     @records = Record.all
   end
 
+  def create
+    @record = Record.new(record_params)
+
+    if @record.save
+      render json: @record
+    else
+      render json: @record.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @record = Record.find(params[:id])
+    @record.destroy
+    head :no_content
+  end
+
+  private
+
+  def record_params
+    params.require(:record).permit(:title, :date, :amount)
+  end
 end
